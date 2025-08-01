ae04fab7486899965cea0c98fe69c27ffd3d9777
Shelly Jain <shellyjain0045@gmail.com>
2025-08-01 13:44:05 +0530

    Update reminder notification manager with latest changes

--- src/lib/reminder-notification-manager.ts
+++ src/lib/reminder-notification-manager.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
 'use client';
 
 import { notificationService } from './notification-service';
